02b14e6a558177f560d20134f29c8215d65a64ff
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2017-07-19 18:26:28 -0500

    Add app.module to import components and services to launch application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ColeccionesComponent } from './colecciones/colecciones.component';
+import { StartComponent } from './start/start.component';
+
+
+
+import { ColeccionesService } from './colecciones.service';
+import { GaleriaService } from './galeria.service';
+import { AppRoutingModule } from './app/app-routing.module';
+
+import { SlickModule } from 'ngx-slick';
+import { ColeccionDetalleComponent } from './coleccion-detalle/coleccion-detalle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColeccionesComponent,
+    StartComponent,
+    ColeccionDetalleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    SlickModule.forRoot()
   ],
-  providers: [],
+  providers: [ColeccionesService,GaleriaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
