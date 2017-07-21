98538bc1415ff21c7084700b0a9193803bcffd71
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2017-07-21 17:24:04 -0500

    Update collection component to import services

--- src/app/colecciones/colecciones.component.ts
+++ src/app/colecciones/colecciones.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { ColeccionesService } from '../colecciones.service';
+import { ColeccionesService } from '../shared/colecciones.service';
 
 @Component({
   selector: 'app-colecciones',
